d707b02a7cd39b0fd9d3cf60cf655848ee3d1666
purple-yr <1419497772@qq.com>
2018-09-29 22:22:50 +0800

    information

--- Bocai/src/js/casejs/information.js
+++ Bocai/src/js/casejs/information.js
@@ -1,12 +1,19 @@
 $(function() {
+    // $(".header").load("../../html/public/header.html");
     $(window).scroll(function() {
         var top = $(window).scrollTop();
-        console.log(top);
+
         $(".banner>.img").css({
             // "top": top++,
             top: top,
         });
+        console.log(top);
     });
 
-
-})
+    var wid = $(window).width();
+    // var hei = $(".banner").height();
+    // console.log(hei);
+    // $(".banner>.img").css({
+    //     width: wid,
+    // })
+});
