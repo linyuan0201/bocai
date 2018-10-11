229cbffa15bf0dabcbfdf2af1a87fa274d4ac89d
purple-yr <1419497772@qq.com>
2018-10-11 11:51:18 +0800

    case 更改

--- Bocai/src/js/casejs/information_ajax.js
+++ Bocai/src/js/casejs/information_ajax.js
@@ -7,50 +7,72 @@ $.ajax({
     dataType: "json",
     success: function(response) {
         // console.log(response);
+        // var str1 = $(".my-gallery-container").html();
+        // console.log(str1);
+        // $.getScript("../../js/casejs/mp.mansory.min.js").remove();
         $(".more-load").on("click", function() {
+
             count = false;
             var output = "";
             if (count == false) {
                 $("#loadmore").css("display", "none");
                 $(".news").css("paddingBottom", "0");
             }
-            for (var i in response) {
-                output += `
-                    <div class="item" data-order=${response[i].s_id}>
-                    <div class="container-fluid box">
-                        <a href="#" class="sign-a">
-                        <img src="${response[i].s_url}">
-                        </a>
-                        <div class="date">
-                            <span class="day">
-                            <p class="ri">${response[i].s_day}</p>
-                            <p class="month">${response[i].s_month}</p>
-                        </span>
-                            <div class="xq">
-                                <p class="dec">
-                                    <a href="#">
-                                    ${response[i].s_decrition}
-                                </a>
-                                </p>
-                                <p class="vid">
-                                    <a href="#">
-                                    ${response[i].s_vi}                                                                          
-                                </a>
-                                </p>
-                            </div>
-                            <a href="#" class="details">
-                                <span class="detail-co">Details</span>
-                                <span class="dec-icon"></span>
+
+            for (var i = 0; i < response.length; i++) {
+                console.log(i);
+                if (i > 8) {
+
+                    // console.log($(".item").parent());
+                    // $(".item").parent().remove();
+                    output = `
+                <div class="item" data-order=${response[i].s_id}>
+                <div class="container-fluid box">
+                    <a href="#" class="sign-a">
+                    <img src="${response[i].s_url}">
+                    </a>
+                    <div class="date">
+                        <span class="day">
+                        <p class="ri">${response[i].s_day}</p>
+                        <p class="month">${response[i].s_month}</p>
+                    </span>
+                        <div class="xq">
+                            <p class="dec">
+                                <a href="#">
+                                ${response[i].s_decrition}
                             </a>
+                            </p>
+                            <p class="vid">
+                                <a href="#">
+                                ${response[i].s_vi}                                                                          
+                            </a>
+                            </p>
                         </div>
+                        <a href="#" class="details">
+                            <span class="detail-co">Details</span>
+                            <span class="dec-icon"></span>
+                        </a>
                     </div>
-                </div>`;
+                </div>
+            </div>`;
+                    if (i % 3 == 0) {
+                        console.log(1);
+                        console.log($(".padding:first"));
+                        $(".padding:first").append(output);
+                    } else if (i % 3 == 1) {
+                        console.log(2);
+                        console.log($(".padding:eq(1)"));
+                        $(".padding:eq(1)").append(output);
+                    } else {
+                        console.log(3);
+                        console.log($(".padding:eq(2)"));
+                        $(".padding:eq(2)").append(output);
+                    }
+                }
             }
-            $(".my-gallery-container").html(output);
-            // console.log(1);
-            $.getScript("../../js/casejs/mp.mansory.min.js");
-            $.getScript("../../js/casejs/pubuliu.js");
         });
+
+
         $(".iconfont1").on("click", function() {
             var str = $(".search1>.input").val();
             var put = "";
