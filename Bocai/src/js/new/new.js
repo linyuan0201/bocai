ecef4c191dc5797cba98edda373f13a7495b6d9a
Chen Hang <836839252@qq.com>
2018-10-12 13:48:23 +0800

    sdfds

--- Bocai/src/js/new/new.js
+++ Bocai/src/js/new/new.js
@@ -80,44 +80,60 @@ $(".form>div button:eq(0)").on("click", function() {
     })
     // 登录按钮
 $(".form>div button:eq(1)").on("click", function() {
-    var usename = $(".form input:eq(0)").val(),
-        password = $(".form input:eq(1)").val(),
-        url = "../../php/new/register2.php",
-        output = $(".form div p");
-    var data = {
-        "usename": usename,
-        "password": password,
-    }
-    if (flag == 1 && flag1 == 1) {
-        $.ajax({
-            type: "post",
-            url: url,
-            data: data,
-            dataType: "json",
-            success: function(response) {
-                console.log(response.pwd);
-                if (response.res == 0) {
-                    output.css({
-                        "display": "block",
-                    }).html("ERROR: INVALID LOGIN");
-                } else if (response.pwd == 1) {
-                    location.href = "../../html/home/home.html";
-                } else {
-                    output.css({
-                        "display": "block",
-                    }).html("ERROR: password error");
+        var usename = $(".form input:eq(0)").val(),
+            password = $(".form input:eq(1)").val(),
+            url = "../../php/new/register2.php",
+            output = $(".form div p");
+
+        var data = {
+            "usename": usename,
+            "password": password,
+        }
+        var dat = new Date();
+        dat.setDate(dat.getDate() + 1);
+        document.cookie = usename + "=" + password + ";expires=" + dat;
+        if (flag == 1 && flag1 == 1) {
+            $.ajax({
+                type: "post",
+                url: url,
+                data: data,
+                dataType: "json",
+                success: function(response) {
+                    console.log(response.pwd);
+                    if (response.res == 0) {
+                        output.css({
+                            "display": "block",
+                        }).html("ERROR: INVALID LOGIN");
+                    } else if (response.pwd == 1) {
+                        location.href = "../../html/home/home.html";
+                    } else {
+                        output.css({
+                            "display": "block",
+                        }).html("ERROR: password error");
+                    }
+                },
+                error: function(err) {
+                    console.log(err);
                 }
-            },
-            error: function(err) {
-                console.log(err);
-            }
-        });
-    } else {
-        output.css({
-            "display": "block",
-        }).html("ERROR: INVALID LOGIN");
-    }
-    flag = 0;
-    flag1 = 0;
-    $(".form>div input").val("");
+            });
+        } else {
+            output.css({
+                "display": "block",
+            }).html("ERROR: INVALID LOGIN");
+        }
+        flag = 0;
+        flag1 = 0;
+        $(".form>div input").val("");
+    })
+    // 
+if (document.cookie) {
+    var cooke_arr = document.cookie.split("; ");
+    var cooke_arr_2 = cooke_arr[cooke_arr.length - 1].split("=");
+    console.log(cooke_arr_2);
+    $(".form input:eq(0)").val(cooke_arr_2[0]);
+    $(".form input:eq(1)").val(cooke_arr_2[1]);
+}
+$(".form input:eq(0)").on("focus", function() {
+    console.log(1);
+    $(".form input:eq(1)").val("");
 })
