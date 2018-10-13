9f189954fbf0000c7ec9728182db76b62bc33751
linyuan0201 <liuyuesgk@163.com>
2018-10-13 15:37:33 +0800

    detail

--- Bocai/src/js/video/load.js
+++
@@ -1,4 +0,0 @@
-$(".header").load("../../html/public/header.html");
-$(".footer").load("../../html/public/footer.html");
-$(".btm_map").load("../../html/public/btm-map.html");
-$(".ri_menu").load("../../html/public/right_menu.html");
--- Bocai/src/js/video/resize.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $(window).resize(function() {
-        var width = $(window).width();
-        console.log(width);
-        if (width < 768) {
-            $(window).removeClass('show');
-        }
-    })
-})
