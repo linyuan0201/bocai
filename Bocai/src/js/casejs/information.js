d63f3a0135a504c63adc4e48c61b962180d42a54
purple-yr <1419497772@qq.com>
2018-10-13 17:46:06 +0800

    hfhjjj

--- Bocai/src/js/casejs/information.js
+++ Bocai/src/js/casejs/information.js
@@ -1,5 +1,5 @@
 $(function() {
-    // $(".header").load("../../html/public/header.html");
+    $(".header").load("../../html/public/header.html");
     $(window).scroll(function() {
         var top = $(window).scrollTop() / 2;
 
@@ -11,22 +11,7 @@ $(function() {
     });
 
     var wid = $(window).width();
-    // var hei = $(".banner").height();
-    // console.log(hei);
-    // $(".banner>.img").css({
-    //     width: wid,
-    // })
-    // $("#loadmore").on("clink", function() {
-    //     $("#loadmore").css("background", "#333").addClass("animated bounceOutLeft").animate({
-    //         "height": "40px",
-    //         "width": "80px",
-    //         "top": "-150px",
-    //         "left": "-200px"
-    //     }, 1000)
-    //     console.log(1);
-    // });
     $(".cebian").load("../../html/public/right_menu.html");
     $(".footer").load("../../html/public/btm-map.html");
     $(".footer2").load("../../html/public/footer.html");
-
 });
